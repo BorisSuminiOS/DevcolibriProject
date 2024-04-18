9feccae6a0a1a45b1e1df419afa44516550483c9
Boris <MTK26@mail.ru>
2024-04-18 14:43:15 +0300

    remove folder logs

--- base/base_class.py
+++ base/base_class.py
@@ -8,29 +8,24 @@ class Base():
         self.driver = driver
 
 
-    '''Получить текущий URL'''
     def get_current_url(self):
         print(f'URL: {self.driver.current_url}')
 
 
-    '''Метод для разделения тестов'''
     def separator_tests(self):
-        print('---------------------------------------------------------')
+        print('------------------------')
 
 
-    '''Метод сравнения элемента по тексту'''
     def method_assert_word(self, word, result):
         assert word == result
         print(f"Verification is successful: '{word}'")
 
 
-    '''Позволяет сделать прокрутку страницы до выбранного элемента'''
     def apply_scrolling_to_element(self, locator):
         action = ActionChains(self.driver)
         action.move_to_element(locator).perform()
 
 
-    '''Позволяет сделать прокрутку по X и Y'''
     def apply_scroll(self, x, y):
         time.sleep(2)
         self.driver.execute_script(f"window.scrollTo({x}, {y})")
@@ -38,7 +33,6 @@ class Base():
         time.sleep(2)
 
 
-    '''Метод для получения скриншота страницы'''
     def to_screenshot(self, name):
         time.sleep(7)
         date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
